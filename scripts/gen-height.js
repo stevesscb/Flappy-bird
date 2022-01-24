7e96e1b1d08272c9d53db6565aff0bbfcf68a209
stevesscb <stevesscb@gmail.com>
2022-01-24 15:37:00 +0800

    5thupdate

---
+++ scripts/gen-height.js
@@ -0,0 +1,34 @@
+const GAME_HEIGHT = 600
+
+// generating the entry hole between the pipes
+// min distance is 70px, max distance 100px
+const genEntryHeight = (min, max)  => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+//generating the total length of pipe
+const genPipesHeight = (entryHeight) => {
+  const allocationHeight = GAME_HEIGHT - entryHeight
+
+  const topHeightPercentage = Math.random().toFixed(2)
+
+  const topHeight = Math.floor(allocationHeight * topHeightPercentage)
+  const botHeight = allocationHeight - topHeight
+
+  return {
+    topHeight,
+    botHeight
+  }
+}
+
+const genHeights = () => {
+  const entryHeight = genEntryHeight(70, 100)
+  const pipeHeight = genPipesHeight(entryHeight)
+
+  return {
+    ...pipeHeight,
+    entryHeight
+  }
+}
+
+export default genHeights
