d3bf40c77a3d603f67994a31f01872a40928c6e3
stevesscb <stevesscb@gmail.com>
2022-01-25 15:16:16 +0800

    Tuesday 3:15pm

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,29 @@
 import genHeight from './gen-height.js'
 
-//
-// GLOBAL CONSTANTS
-//
+// GLOBAL | CONSTANTS
 const GAME_HEIGHT = 600
 const GAME_WIDTH = 600
 const CHARACTER_WIDTH = 50
 const CHARACTER_HEIGHT = 50
 const FPS = 60
 const LOOP_INTERVAL = Math.round(1000 / FPS)
-const CHARACTER_VELOCITY = 2.5
+const CHARACTER_VELOCITY = 4.5
 const GRAVITY_VELOCITY = 1.5
 const BLOCK_VELOCITY = 1.5
 
-//
-// GLOBAL Game Variables
-//
+// GLOBAL | Game Variables
 let blockLoop
 let gameLoop
 const $gameScreen = $('#game-screen')
 
-//
-// Character
-//
+// Character | Object
 const character = {
   $elem: $('<div id="character"></div>'),
   position: { x: 100, y: 275 },
   movement: { up: false, down: false }
 }
 
-// Toggle which direction the character is moving to
+// Character | Toggle which direction the character is moving to
 const setCharacterMovement = (value, keyCode) => {
   switch (keyCode) {
     case 32:
@@ -42,37 +36,45 @@ const setCharacterMovement = (value, keyCode) => {
   }
 }
 
-// Handling Key Down
+// Character | Handling Key Down
 const handleKeyDown = (e) => {
   setCharacterMovement(true, e.keyCode)
 }
 
-// Handling Key Up
+// Character | Handling Key Up
 const handleKeyUp = (e) => {
   setCharacterMovement(false, e.keyCode)
 }
 
-// Every time this gets invoked, update character position
+// Character | Every time this gets invoked, update character position
 const updateCharacterMovements = () => {
-  const { position: { x, y }, movement: { up, down } } = character
+  const { position: { x, y }, movement: { up } } = character
   let newY = y
 
-// character moving up and falling down
   if (up) {
-    newY -= CHARACTER_VELOCITY + 2;
+    if (y - CHARACTER_VELOCITY <= 0) {
+      newY = 0
+      stopGame()
+    } else {
+      newY -= CHARACTER_VELOCITY
+    }
   } else {
-    newY += GRAVITY_VELOCITY
+    if (y + GRAVITY_VELOCITY >= 550) {
+      newY = 550
+      stopGame()
+    } else {
+      newY += GRAVITY_VELOCITY
+    }
   }
 
   character.position.y = newY
   character.$elem.css('left', x).css('top', newY)
 }
 
-//
-// Blocks
-//
+// Blocks | Array
 const blocks = []
 
+// Blocks | Block Generation
 const generateNewBlocks = () => {
   // * generate blocks heights
   const newBlockHeights = genHeight()
@@ -99,6 +101,7 @@ const generateNewBlocks = () => {
   blocks.push(topBlock, botBlock)
 }
 
+// Blocks | Movement
 const updateBlocksMovements = () => {
   for (let i = blocks.length - 1; i >= 0; i--) {
     const block = blocks[i]
@@ -115,13 +118,10 @@ const updateBlocksMovements = () => {
   }
 }
 
-const init = () => {
+// Game | Start Game
+const startGame = () => {
   // Add Character To Screen
   character.$elem.appendTo($gameScreen)
-
-  // Add key event listeners
-  $(document).on('keydown', handleKeyDown)
-  $(document).on('keyup', handleKeyUp)
 
   // Start the game loop
   gameLoop = setInterval(() => {
@@ -136,6 +136,21 @@ const init = () => {
   blockLoop = setInterval(() => {
     generateNewBlocks()
   }, 6000)
+}
+
+// Game | Stop Game
+const stopGame = () => {
+  clearInterval(blockLoop)
+  clearInterval(gameLoop)
+}
+
+// Game | Initialization
+const init = () => {
+  // Add key event listeners
+  $(document).on('keydown', handleKeyDown)
+  $(document).on('keyup', handleKeyUp)
+
+  // startGame()
 }
 
 init()
